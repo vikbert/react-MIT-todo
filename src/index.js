b2ce34a5ea372f75e35d7e6cb8d2fe448ac30f76
Xun Zhou <xun.zhou@lidl.com>
2019-03-15 01:35:09 +0100

    register todoStorage to window object

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const STORAGE_KEY = 'REACT_APP_TODO_LIST';
+window.todoStorage = {
+  uid: 0,
+  fetch: function () {
+    let todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+
+    localStorage.uid = todos.length;
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+
+    return todos;
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  },
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
