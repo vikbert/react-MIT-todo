059fe25e3db0e3fc9dbfc87323b51276cb82daee
Xun Zhou <xun.zhou@lidl.com>
2019-03-18 21:35:28 +0100

    create functional react component

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,20 +1,9 @@
-import React, {Component} from "react";
+import React from 'react';
 
-class TodoCounter extends Component {
-  counterActive;
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <span className="todo-count">
-        <strong>{this.props.counterActive}</strong> items left
-      </span>
-    );
-  }
-}
+const TodoCounter = (props) => (
+  <span className="todo-count">
+    <strong>{props.counterActive}</strong> items left
+  </span>
+);
 
 export default TodoCounter;
