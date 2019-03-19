33fbdedf784d0cf18b431069b15e4b3093bda1f6
Xun Zhou <xun.zhou@lidl.com>
2019-03-19 16:34:54 +0100

    handle new action in reducer

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -2,13 +2,32 @@ import * as types from '../actions/types';
 
 const initialState = {
   todos: [],
-  visibility: 'all'
+  visibility: 'all',
+};
+
+const removeCompletedTodos = (state) => {
+  return state.todos.filter(todo => todo.completed === false);
+};
+
+const updateTodo = (state, action) => {
+  const todos = [...state.todos];
+
+  todos[todos.indexOf(action.oldTodo)] = action.newTodo;
+
+  console.log(todos)
+  return todos;
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case types.FETCH_TODOS:
       return {...state, todos: action.todos};
+    case types.CREATE_TODO:
+      return {...state, todos: [action.todo, ...state.todos]};
+    case types.UPDATE_TODO:
+      return {...state, todos: updateTodo(state, action)};
+    case types.REMOVE_COMPLETED_TODOS:
+      return {...state, todos: removeCompletedTodos(state)};
     case types.UPDATE_VISIBILITY:
       return {...state, visibility: action.visibility};
     default:
