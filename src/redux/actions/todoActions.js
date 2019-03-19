4b5d1e2d7794e681c21121e952f18b9d7072f364
Xun Zhou <xun.zhou@lidl.com>
2019-03-19 16:35:05 +0100

    add new actions

--- src/redux/actions/todoActions.js
+++ src/redux/actions/todoActions.js
@@ -9,6 +9,28 @@ export const fetchTodos = () => dispatch => {
   });
 };
 
+export const createTodo = (todo) => dispatch => {
+  dispatch({
+    type: types.CREATE_TODO,
+    todo: todo,
+  });
+};
+
+export const updateTodo = (newTodo, oldTodo) => dispatch => {
+  dispatch({
+    type: types.UPDATE_TODO,
+    newTodo: newTodo,
+    oldTodo: oldTodo,
+  });
+};
+
+export const removeCompletedTodos = () => dispatch => {
+  dispatch({
+    type: types.REMOVE_COMPLETED_TODOS,
+  });
+};
+
+
 export const updateVisibility = (visibility) => dispatch => {
   dispatch({
     type: types.UPDATE_VISIBILITY,
