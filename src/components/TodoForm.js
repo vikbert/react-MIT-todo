a5b75d3067d2574cf4781c6fd37b5d16eef519d9
Xun Zhou <xun.zhou@lidl.com>
2019-03-20 14:37:56 +0100

    refactor the form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,41 +1,48 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {createTodo} from "../redux/actions/todoActions";
+import PropTypes from 'prop-types';
 
 class TodoForm extends Component {
-  state = {todoInput: ''};
+  state = {inputText: ''};
 
-  handleChange = event => {
-    this.setState({todoInput: event.target.value});
+  handleChange = e => {
+    this.setState({inputText: e.target.value});
   };
 
-  handleKeyPress = event => {
-    if (event.key === 'Enter') {
-      const text = this.state.todoInput.trim();
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      const text = this.state.inputText.trim();
       if (!text) {
         return;
       }
 
       const newTodo = {
         id: window.todoStorage.uid++,
-        title: this.state.todoInput,
+        title: this.state.inputText,
         starred: 0,
         completed: false,
       };
 
       this.props.createTodo(newTodo);
-      this.setState({todoInput: ''});
+      this.setState({inputText: ''});
     }
   };
 
   render() {
     return (
       <input className="new-todo" type="text" placeholder="What needs to be done?"
-             value={this.state.todoInput}
+             value={this.state.inputText}
              onChange={this.handleChange}
-             onKeyPress={this.handleKeyPress}/>
+             onKeyPress={this.handleKeyPress}
+             autoFocus={true}/>
     );
   }
 }
+
+TodoForm.propTypes = {
+    inputText: PropTypes.string.isRequired,
+};
+
 
 export default connect(null, {createTodo})(TodoForm);
